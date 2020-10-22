3d55735654360418c543754c5fa86c8d5318e1ca
Chaise Matev <aj_fl81@msn.com>
2020-10-22 14:24:51 -0400

    firebase init

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,10 @@
+export default firebaseConfig = {
+  apiKey: 'AIzaSyAGe7bYQHvP1f5iCpZl6e0P0T_8DNJUuKo',
+  authDomain: 'kontaques-eb60e.firebaseapp.com',
+  databaseURL: 'https://kontaques-eb60e.firebaseio.com',
+  projectId: 'kontaques-eb60e',
+  storageBucket: 'kontaques-eb60e.appspot.com',
+  messagingSenderId: '1008979975986',
+  appId: '1:1008979975986:web:6cff4008d84310e211c12b',
+  measurementId: 'G-9XFBRJP6WH',
+};
