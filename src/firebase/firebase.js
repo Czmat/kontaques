f9b94eb16944fd82c6e1faf70fe619a8807e3239
Daniel Mamnev <danielmamnev@gmail.com>
2020-10-22 16:39:39 -0400

    added firebase and edited config

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -3,4 +3,11 @@ import firebaseConfig from './firebaseConfig';
 
 firebase.initializeApp(firebaseConfig);
 
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.addScope('https://mail.google.com/');
+
+export function signIn() {
+  firebase.auth().signInWithPopup(provider);
+}
+
 export default firebase;
