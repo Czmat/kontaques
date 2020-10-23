c806f5db123e6e56a9fceee0c56b6ac78f245a8b
danielmamnev <danielmamnev@gmail.com>
2020-10-23 13:34:16 -0400

    gapi sign in alongside firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,10 +4,18 @@ import firebaseConfig from './firebaseConfig';
 firebase.initializeApp(firebaseConfig)
 console.log('firebase initialized')
 const provider = new firebase.auth.GoogleAuthProvider();
+
+
 provider.addScope('https://mail.google.com/');
 
 export function signIn() {
-  firebase.auth().signInWithPopup(provider);
+const GoogleAuth = window.gapi.auth2.getAuthInstance();
+
+  firebase.auth().signInWithPopup(provider).then(() => {
+    if (!window.gapi.auth2.getAuthInstance().isSignedIn.get()) {
+      GoogleAuth.signIn();
+    }
+  });
 }
 
 export default firebase;
