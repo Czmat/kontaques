e7aa95b0e555ebb41bda94f2bc99f1aafbfa011d
Chaise Matev <aj_fl81@msn.com>
2020-10-27 14:13:40 -0400

    contact create in progress

--- src/components/createContacts.js
+++ src/components/createContacts.js
@@ -1,19 +1,33 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import firebase from '../firebase/firebase';
 
+const initial_contact = {
+  firstName: '',
+  lastname: '',
+  imgUrl: '',
+  email: '',
+  phone: '',
+  occupation: '',
+  discriptionNote: '',
+  address: '',
+};
+
 const CreateContacts = ({ contacts, auth, dispatch }) => {
-  console.log('auth', auth.auth.uid);
-  const [contact, setContact] = useState({
-    firstName: '',
-    lastname: '',
-    imgUrl: '',
-    email: '',
-    phone: '',
-    occupation: '',
-    discriptionNote: '',
-    address: '',
-  });
+  // console.log('auth', auth.auth.uid);
+  const [contact, setContact] = useState(initial_contact);
+
+  useEffect(() => {
+    contactCollection.get().then((snapshot) => {
+      const data = snapshot.docs.map((d) => d.data());
+      console.log('snapshot', data);
+      dispatch({ type: 'GET_CONTACTS', payload: data });
+    });
+  }, []);
+
+  const contactCollection = firebase
+    .firestore()
+    .collection(`users/${auth.auth.uid}/contacts`);
 
   const onChange = (e) => {
     setContact({ ...contact, firstName: e.target.value });
@@ -21,12 +35,25 @@ const CreateContacts = ({ contacts, auth, dispatch }) => {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    dispatch({ type: 'CREATE_CONTACT', payload: contact });
+    // dispatch({ type: 'CREATE_CONTACT', payload: contact });
+    createContactInFirestore();
   };
 
   const createContactInFirestore = () => {
-    const contactCollection = firebase.firestore().collection('contacts');
-    // const contact
+    if (!contact.imgUrl) {
+      contactCollection
+        .doc()
+        .set({ ...contact, id: contactCollection.doc().id })
+        .then(() => {
+          console.log('Contact has been created');
+          setContact(initial_contact);
+          contactCollection.get().then((snapshot) => {
+            const data = snapshot.docs.map((d) => d.data());
+            // console.log('snapshot', data)
+            dispatch({ type: 'GET_CONTACTS', payload: data });
+          });
+        });
+    }
   };
 
   // console.log('contacts from redux', contacts.contacts);
@@ -38,7 +65,7 @@ const CreateContacts = ({ contacts, auth, dispatch }) => {
       </form>
       <ul>
         {contacts.contacts.map((contact) => {
-          return <li key={contact.firstName + 1}>{contact.firstName}</li>;
+          return <li key={contact.id}>{contact.firstName}</li>;
         })}
       </ul>
     </div>
--- src/redux/contactsReducer.js
+++ src/redux/contactsReducer.js
@@ -2,6 +2,8 @@ export default (state = { contacts: [] }, action) => {
   switch (action.type) {
     case 'CREATE_CONTACT':
       return { ...state, contacts: [...state.contacts, action.payload] };
+    case 'GET_CONTACTS':
+      return (state = { contacts: action.payload });
     default:
       return state;
   }
