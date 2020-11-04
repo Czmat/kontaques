c79d0e8cec965c143f2f911350f7fd302bac3de1
devmikev <dev.mikev@gmail.com>
2020-11-03 22:08:03 -0500

    added mobile nav modal

--- src/components/UI/Header/Header.js
+++ src/components/UI/Header/Header.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { connect } from 'react-redux';
 import { useHistory } from 'react-router-dom';
 // import '../css/style.css';
@@ -6,6 +6,7 @@ import styles from './Header.module.css';
 import firebase from '../../../firebase/firebase';
 
 function Header({ auth, dispatch }) {
+  const [showModal, setShowModal] = useState(false);
   let history = useHistory();
 
   const Login = () => {
@@ -14,6 +15,10 @@ function Header({ auth, dispatch }) {
 
   const goTo = (page) => {
     history.push('/' + page);
+  };
+
+  const toggleModal = () => {
+    setShowModal(!showModal);
   };
 
   const Logout = () => {
@@ -28,10 +33,40 @@ function Header({ auth, dispatch }) {
 
   return (
     <header className={styles.main_header}>
+      {showModal ? (
+        <div>
+          <div onClick={toggleModal} className={styles.backdrop}></div>
+          <nav className={styles.mobile_nav}>
+            <ul className={styles.mobile_nav__items}>
+              <li className={styles.mobile_nav__item}>
+                <a
+                  onClick={() => {
+                    goTo('dashboard');
+                    toggleModal();
+                  }}
+                >
+                  Dashboard
+                </a>
+              </li>
+              <li className={styles.mobile_nav__item}>
+                <a
+                  onClick={() => {
+                    goTo('send');
+                    toggleModal();
+                  }}
+                >
+                  Send an Email
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      ) : null}
+
       {auth.auth ? (
         <div>
           <div className={styles.main_header__welcome}>
-            <button className={styles.toggle_button}>
+            <button className={styles.toggle_button} onClick={toggleModal}>
               <span className={styles.toggle_button__bar}></span>
               <span className={styles.toggle_button__bar}></span>
               <span className={styles.toggle_button__bar}></span>
